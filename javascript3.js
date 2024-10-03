e95c56c7e524a91a8c02c4cb617512b8c1b02244
Renan-Jorge-Meireles <159266744+Renan-Jorge-Meireles@users.noreply.github.com>
2024-10-03 16:19:48 -0300

    03-10
    coloquei comentario, deixei umas coisas mais bonita, fiz algumas mudança nos codigos

--- javascript3.js
+++ javascript3.js
@@ -1,3 +1,5 @@
+//VF = valor financiado
+//Ndep = numero de parcelas
 function calcula() {
     const VF = parseFloat(document.getElementById("num1").value);
     const taxa = parseFloat(document.getElementById("num2").value) / 100;
@@ -7,11 +9,12 @@ function calcula() {
         document.getElementById("resultado").innerText = "Erro: Digite Um Número";
         return;
     }
-
+//p = valor que será exibido
     const p = (VF * taxa * Math.pow(1 + taxa, Ndep)) / (Math.pow(1 + taxa, Ndep) - 1);
     document.getElementById("resultado").innerText = `Valor da Parcela: R$ ${p.toFixed(2)}`;
 }
 
+//substitui o texto pelo que está dentro das aspas abaixo quando o botao de limpar é clicado
 function limpar() {
     document.getElementById("num1").value = '';
     document.getElementById("num2").value = '';
